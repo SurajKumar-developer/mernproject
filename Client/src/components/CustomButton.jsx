100d1838b82b1e04b7c754dc56278a05c76483e5
Suraj Kumar <surajpandey31081@gmail.com>
2024-02-20 10:51:15 +0530

    first commit

---
+++ Client/src/components/CustomButton.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+import {} from "react";
+
+const CustomButton = ({ title, containerStyles, iconRight, type, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type || "button"}
+      className={`inline-flex items-center ${containerStyles}`}
+    >
+      {title}
+
+      {
+        iconRight && <div className="ml-2">{iconRight}</div>
+      }
+    </button>
+  );
+};
+
+export default CustomButton;
